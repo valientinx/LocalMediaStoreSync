4ebf7e253631eda3a7f466189e9f5e5dff16da15
office.valentin <office.valentin@gmail.com>
2023-09-14 18:11:17 +0300

    Introducing compose UI

--- app/src/main/java/com/my/homecloud/MainActivity.kt
+++ app/src/main/java/com/my/homecloud/MainActivity.kt
@@ -2,10 +2,18 @@ package com.my.homecloud
 
 import android.Manifest
 import android.content.IntentSender
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.my.homecloud.ui.main.MainFragment
-import com.my.homecloud.ui.main.MainViewModel
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecodelab.ui.theme.LocalMediaStoreSyncTheme
+import androidx.compose.ui.tooling.Com
 
 /** The request code for requesting [Manifest.permission.READ_EXTERNAL_STORAGE] permission. */
 private const val READ_EXTERNAL_STORAGE_REQUEST = 0x1045
@@ -15,17 +23,35 @@ private const val READ_EXTERNAL_STORAGE_REQUEST = 0x1045
  */
 private const val DELETE_PERMISSION_REQUEST = 0x1033
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
 //    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        setContent {
+            LocalMediaStoreSyncTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        LocalMediaStoreSyncTheme {
+            Greeting("Android")
         }
     }
 }
--- app/src/main/java/com/my/homecloud/ui/main/MediaStoreImage.kt
+++ app/src/main/java/com/my/homecloud/ui/main/MediaStoreImage.kt
@@ -17,7 +17,7 @@
 package com.my.homecloud.ui.main
 
 import android.net.Uri
-import androidx.recyclerview.widget.DiffUtil
+//import androidx.recyclerview.widget.DiffUtil
 import java.util.*
 
 /**
@@ -30,12 +30,12 @@ data class MediaStoreImage(
     val contentUri: Uri
 ) {
     companion object {
-        val DiffCallback = object : DiffUtil.ItemCallback<MediaStoreImage>() {
-            override fun areItemsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
-                oldItem.id == newItem.id
-
-            override fun areContentsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
-                oldItem == newItem
-        }
+//        val DiffCallback = object : DiffUtil.ItemCallback<MediaStoreImage>() {
+//            override fun areItemsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
+//                oldItem.id == newItem.id
+//
+//            override fun areContentsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
+//                oldItem == newItem
+//        }
     }
 }
