d5f65baba7dedd85ebff490764461cb97c55c070
office.valentin <office.valentin@gmail.com>
2024-01-26 16:45:41 +0200

    persistent and state in composition

--- README.md
+++ README.md
@@ -36,4 +36,4 @@ https://kotlinlang.org/docs/getting-started.html
 
 
 InProgress:
-https://developer.android.com/codelabs/jetpack-compose-state?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-state#2
+https://developer.android.com/codelabs/jetpack-compose-state?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-state#6
--- app/src/main/java/com/my/homecloud/ui/watercounter/WaterCounter.kt
+++ app/src/main/java/com/my/homecloud/ui/watercounter/WaterCounter.kt
@@ -1,19 +1,35 @@
 package com.my.homecloud.ui.watercounter
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember {mutableStateOf(0)}
+        if (count > 3) {
+            Text(
+                text = "You've had ${count} glasses.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
 }
 
 @Composable
